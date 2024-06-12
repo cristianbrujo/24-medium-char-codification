ef605f9c9158de8ae6cf2b70905acf1b361d7a52
Cristian <fake@ema.il>
2024-06-12 13:27:41 +0200

    Conversión de ASCII a UTF-8

--- 02-ascii2utf8.js
+++ 02-ascii2utf8.js
@@ -3,3 +3,8 @@ function asciiAUtf8(caracter) {
     let utf8Hex = codigoAscii.toString(16);
     return utf8Hex;
 }
+
+console.log(asciiAUtf8("A")); // 41
+console.log(asciiAUtf8("B")); // 42
+console.log(asciiAUtf8("C")); // 43
+console.log(asciiAUtf8("D")); // 44
