dd2d52affd87bb151f5e202d522f0fdda45b4bea
Cristian <fake@ema.il>
2024-06-12 13:01:16 +0200

    Conversión de Entero a ASCII en JavaScript

--- 01-ascii.js
+++ 01-ascii.js
@@ -1,3 +1,7 @@
-let numero = 65;
+function randInt(max) {
+    return Math.floor(Math.random() * (max + 1))
+}
+
+let numero = randInt(255);
 let caracter = String.fromCharCode(numero);
 console.log(caracter);  // Output: A
